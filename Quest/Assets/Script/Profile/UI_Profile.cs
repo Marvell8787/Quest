9602cf0b05244760fe1edfa4c403558467923112
Marvell8787 <wendy850122@gmail.com>
2020-02-12 10:32:32 +0800

    20200212

---
+++ Quest/Assets/Script/Profile/UI_Profile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class UI_Profile : MonoBehaviour {
+
+    public Button Back_btn;
+
+    public AudioSource choose, ok, cancel;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void Back()
+    {
+        ok.Play();
+        SceneManager.LoadScene("Home");
+    }
+}
---
+++ Quest/Assets/Script/Rank/UI_Rank.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class UI_Rank : MonoBehaviour {
+
+    public Button Back_btn;
+
+    public AudioSource choose, ok, cancel;
+
+    // Use this for initialization
+    void Start () {
+        Back_btn.onClick.AddListener(Back);
+
+    }
+    void Back()
+    {
+        ok.Play();
+        SceneManager.LoadScene("Home");
+    }
+}
