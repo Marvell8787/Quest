3ba152d6f705213e750edc18a2eeb13bb84df101
Marvell8787 <46806099+Marvell8787@users.noreply.github.com>
2020-03-18 14:32:15 +0800

    20200318

---
+++ Quest/Assets/DataBase/Savelog/Behaviour_Bank.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Behaviour_Bank{
+    public static string[] GameBehavior_Task = new string[9]{"GA","GAA","GAB","GAC","GAD","GAE","GAF","GAG","GAH"};
+    public static string[] GameBehavior_Battle = new string[5] { "GB", "GBA", "GBB", "GBC", "GBD"};
+
+    public static string LearnBehavior_Learn = "LA"; 
+    public static string[] LearnBehavior_Material = new string[4] { "LB", "LBA", "LBB", "LBC" };
+    public static string[] LearnBehavior_Level = new string[5] { "LC", "LCA", "LCB", "LCC", "LCD" };
+
+    public static string[] SupportBehavior_Guide = new string[3] { "SA", "SAA", "SAB"};
+    public static string[] SupportBehavior_Profile = new string[3] { "SB", "SBA", "SBB"};
+    public static string[] SupportBehavior_Deck = new string[3] { "SC", "SCA", "SCB" };
+    public static string[] SupportBehavior_Rank = new string[2] { "SD", "SDA"};
+    public static string[] SupportBehavior_Badge = new string[2] { "SE", "SEA"};
+    public static string[] SupportBehavior_Shop = new string[3] { "SF", "SFA", "SFB" };
+}
